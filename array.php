44055c484bea7fd99649ad81e798e827ceace2df
usernamedinesh <borod9200@gmail.com>
2025-07-31 13:08:09 +0530

    array accessing, and key value pairs

--- array.php
+++ array.php
@@ -1,3 +1,19 @@
 <?php
 echo "array php\n";
+$fruits = ["banana", "mango", "apple"];
+echo $fruits[1]; // mango
+$fruits[2] = "pineapple\n";
+echo "\n";
+echo $fruits[2]; // pineapple
+
+//key value paris 
+$person = [
+    "first_name" => "John",
+    "last_name" => "Doe",
+    "age" => 30
+];
+echo $person["first_name"]; 
+echo "\n";
+echo "age : " . $person["age"]; 
+echo "\n";
 ?>
