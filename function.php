0ba1f4a75b84c699b0213d9a1295213882448342
usernamedinesh <borod9200@gmail.com>
2025-07-31 14:51:51 +0530

    passing argument

--- function.php
+++ function.php
@@ -1,4 +1,18 @@
 <?php
+/* function declaration */
+function greeting($name) {
+    echo "Hello, " . $name;
+}
+greeting("dinesh");
+echo "\n";
 
-echo "fuction\n";
+
+/* passing argument */
+function addNumbers($num1, $num2) {
+  $sum = $num1 + $num2;
+  return $sum;
+}
+
+echo addNumbers(3, 4);
+echo "\n";
 ?>
