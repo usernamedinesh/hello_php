4aab6d70f479078b0f88d71fae1e537f23055269
usernamedinesh <borod9200@gmail.com>
2025-07-31 14:54:14 +0530

    dafult argument

--- function.php
+++ function.php
@@ -6,13 +6,22 @@ function greeting($name) {
 greeting("dinesh");
 echo "\n";
 
-
 /* passing argument */
 function addNumbers($num1, $num2) {
   $sum = $num1 + $num2;
   return $sum;
 }
-
 echo addNumbers(3, 4);
+echo "\n";
+
+/* default argument */
+function greet($name = "guest"){
+    echo "hello " . $name;
+}
+
+greet();
+echo "\n";
+greet("John");
+
 echo "\n";
 ?>
