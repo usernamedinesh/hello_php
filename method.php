9e4b5619fe7ccf4c4b9e0cbe60ccd457a7b98975
usernamedinesh <borod9200@gmail.com>
2025-08-01 18:16:57 +0530

    addign some method or function

---
+++ method.php
@@ -0,0 +1,11 @@
+<?php
+echo "PHP method\n";
+/* isset($var) rturn true if the variable has a value otherwise nothing*/ 
+/* unset($var) remove the value*/ 
+/* var_dump($var) return data type */
+/* is_int($var) return rturn true if int*/
+/* is_bool($var) return true if bool */
+/* is_string($var) return true if string */
+/* is_array($var) return true if array */
+/* is_object($var) return true if object */
+?>
