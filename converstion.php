93be04594f261c308d399a2a60790eec9a121472
usernamedinesh <borod9200@gmail.com>
2025-08-01 18:23:26 +0530

    output statments

---
+++ converstion.php
@@ -0,0 +1,13 @@
+<?php
+$x = "hello";
+$x = (int)$x;
+/* echo $x; // return 0 */
+/* $x = (bool)$x; return 1 */
+
+/* output statement */
+$var1 = "hello\n";
+$var2 = "world\n";
+echo $var1, $var2;
+
+/* print statement return always true after printing */
+?>
