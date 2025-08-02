d5e8227396f8d4fc6f4e3eb99a424d5f086e7a6f
usernamedinesh <borod9200@gmail.com>
2025-08-02 13:04:32 +0530

    add 4 type string

--- string.php
+++ string.php
@@ -1,3 +1,25 @@
 <?php
 echo "string\n";
+/*
+ * 4-ways to declare a string
+      $str1 = 'single quote';
+      $str2 = "double quote";
+      $str3 = "double quote";
+      $str4 = "double quote";
+*/
+$name = "Dinesh";
+$heredocString = <<<EOT
+This is a multiline string
+using heredoc syntax.
+Variables like $name are interpolated.
+EOT;
+
+$nowdocString = <<<'ABC'
+This is a multiline string
+using nowdoc syntax.
+Variables like $name are NOT interpolated.
+ABC;
+
+echo $heredocString;
+echo $nowdocString;
 ?>
