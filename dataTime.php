f710fb6ebbc8198d492eda12597b7d0586eb1645
usernamedinesh <borod9200@gmail.com>
2025-08-02 13:49:56 +0530

    adding different format of date

--- dataTime.php
+++ dataTime.php
@@ -2,6 +2,21 @@
 /*
  
 */
-echo "date time\n";
+echo "\n<br>";
+echo date("l"); // Saterday
+echo "\n<br>";
+echo date("D"); // Sat
+echo "\n<br>";
+echo date("F"); // Augest
+echo "\n<br>";
+echo date("M"); // Aug(mounth)
+echo "\n<br>";
+echo date("d"); // 02(day)
+echo "\n<br>";
+echo date("m"); // 08(mounth)
+echo "\n<br>";
+echo date("l d-m-Y\n");  // y = 25, Y = 2025
+echo "\n<br>";
+
 
 ?>
