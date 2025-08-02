5d5f16fc9880d13d7c773ec17e355e1a7dac0958
usernamedinesh <borod9200@gmail.com>
2025-08-02 14:04:13 +0530

    time

--- dataTime.php
+++ dataTime.php
@@ -19,4 +19,27 @@ echo date("l d-m-Y\n");  // y = 25, Y = 2025
 echo "\n<br>";
 
 
+/* Time */
+/*
+ * hour
+  00, 11 -> h, g
+  00, 25 -> H, G 
+
+    minutes  -> i 
+    second   -> s 
+    AM or PM -> A
+    am or pm -> a
+*/
+echo date_default_timezone_get();
+echo date("h:i:s A");
+
+echo "\n<br>";
+
+date_default_timezone_set("Asia/Kolkata");
+echo date("h:i:s A");
+
+echo "\n<br>";
+echo time(); //current time in no of second
+echo "<br>converting :  \n<br>";
+echo date("Y-m-d  h:i:s A", time());
 ?>
