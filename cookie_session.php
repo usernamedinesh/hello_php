bbf2537aa894a2302dfc54e31af519e0535e9556
usernamedinesh <borod9200@gmail.com>
2025-08-02 16:02:46 +0530

    cookie detils

--- cookie_session.php
+++ cookie_session.php
@@ -1,12 +1,19 @@
 <?php
-echo "cookie & session\n";
+echo "cookie & session super global variable\n";
 /*
  * 1. statefull protocol 
     remember previous data
     eg: ftp, tcp/ip
 
- * 2. stateless protocol 
+ * 2. stateless protocol (so use cookie)
     can't remember previous data
     eg: http, htpps
+  
+*/ 
+
+/*
+ * cookie : is a small amount of data that we can store in brower(destoyed when close browser)(not sucure)
+  set cookie: setcookie("name" "value"); 
+  get cookie: $_COOKIE["name"]; 
 */ 
 ?>
