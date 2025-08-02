78487e9fe63565b7d8bc38e260d220afaf07e545
usernamedinesh <borod9200@gmail.com>
2025-08-02 15:54:34 +0530

    protocols

--- cookie_session.php
+++ cookie_session.php
@@ -1,3 +1,12 @@
 <?php
 echo "cookie & session\n";
+/*
+ * 1. statefull protocol 
+    remember previous data
+    eg: ftp, tcp/ip
+
+ * 2. stateless protocol 
+    can't remember previous data
+    eg: http, htpps
+*/ 
 ?>
