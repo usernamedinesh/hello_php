f75b085e7ca8298b8e38884ac2907843edc9fe08
usernamedinesh <borod9200@gmail.com>
2025-08-02 17:46:23 +0530

    add static member

--- oop/class.php
+++ oop/class.php
@@ -1,3 +1,55 @@
 <?php
-echo "class\n";
+/* echo "class\n"; */
+/*
+ * A class is a collection of methods and properties
+*/
+class Sample 
+{
+    public $name = "dinesh";
+    function greeting()
+    {
+        echo "Greeing from function !";
+    }
+}
+
+class StaticClass
+{
+
+    static $x = 10;
+    static function welcome() 
+    {
+        echo  "<p> This is welcome static function </p>";
+    }
+}
+
+/*
+ * Object: instance of an class (allocating the memory to the class members)
+ * through the object we can access class members
+ * create object using new keyword
+*/
+$obj = new Sample;
+$obj->greeting();
+echo "<br>";
+echo $obj->name;
+
+/*
+ * TWO TYPE MEMBER
+   1. static 
+     declared using static keyword, access using class name,
+     get memory allocated at the time of class loading,
+
+   2. non-static
+        by default it is non-static, access using object, 
+        get memory allocated at the time of object creation
+ 
+*/
+
+StaticClass::welcome();
+echo StaticClass::$x;
+
+
+/* SCOPE RESOLUTION OPERATOR (::)
+   using this can access static members  and constant of class
+*/
+
 ?>
