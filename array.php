80560eea3a4bfb015d64ca7283863ff6ca7446ca
usernamedinesh <borod9200@gmail.com>
2025-08-02 12:50:26 +0530

    array done

--- array.php
+++ array.php
@@ -64,6 +64,54 @@ krsort($var) : sorting key in reverse order
 
 * array_combine(arr1, arr2) : arr1 => key | arr2 = value 
 * only pass two args and both arg1 and arg2 have same no of emenet
+*
+* arra_merge(arg1 , arg2, ...) : merge all values of arguments
+*
+* array_unique($var) : remove all dublicate
+*
+* seaching or finding element in array
+* in_array(element , $var) // return true if exist 
+* array_search(element , $var) // return index(position) if found  
+*
+* for seaching key 
+    array_key_exists(key , $var)  // return true if exist 
+*/
+
+/*
+     array_push():  adding element at last  
+     array_unshift():  adding element at first  
+* push and unshift  : return the total no of element(count) after adding
+     array_pop() :   removing element at last  
+     array_shift()  :  removing element at first  
+* pop and shift  : return the deleted element
+
+
+$arr = [10,20,30] // 3 element
+echo array_push($arr, 40) // return 4 
+echo array_unshift($arr, 40) // return 4 
+echo array_unshift($arr, [10.1, 10.2]) // return 4  ( we cana do this to)
+
+echo array_pop($arr); // return 30 (removed element);
+echo array_shift($arr); // return 10 (removed element);
+
+empty aray and pushing
+$al = [] // declaring empty array
+$al [] = 10;
+$al [] = 20;
+$al [] = 30;
+
+or dynamicarrly
+for($i = 91; $i <= 100; i++) {
+  $al[] = $i;
+}
+
+array_splice
+$arr [10,20,30,40];
+$an = array_splice($arr, 2); //an =  10, 20 , 30 & arr = 40
+array_splice($arr, 2, 0); //  arr = nothing remove from arr
+array_splice($arr, 2, 1); //  remove 1 element from the index 2  = 10,20,40
+array_splice($arr, 2, 0, 50); //  remove 1 element from the index 2  = 10,20,50,40
 
 */
+
 ?>
