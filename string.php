277f7ad1194ae0120d33603b438dad19942d6523
usernamedinesh <borod9200@gmail.com>
2025-08-02 13:31:38 +0530

    adding bouch of string method

--- string.php
+++ string.php
@@ -22,4 +22,37 @@ ABC;
 
 echo $heredocString;
 echo $nowdocString;
+/*
+ * METHOD
+ * echo strlen($var) return legth including spaces
+ * echo strtoupper($var) uppercase 
+ * echo strtolower($var) lowercase
+ * echo ucwords($var) starting letter of every words will capital not works if all are capital
+
+ * if all wors are caiptral then we convert it to lower then use ucwors
+ * ucwords(strtolower($var));
+ * ucfirst($var)  : only first letter will be capital
+ *
+ * EXPLODE(string to array) and IMPLODE (arr to string)
+ * $str1 = "1#2#3#";
+ * $arr = explode("#", $str1);  :=>1, 2 ,3 
+ * $arr = [10,20,30]; 
+ * $str1 =  implode("#", $arr);  10#20#30# 
+ *
+ * $email = "borod@gmail.com";
+ * echo strpos($email, "@"); //return position of first occurence
+ * echo stripos($email, "@"); //return position of first occucence case insensetive (a and A is same)
+ *
+ * echo substr($email, 3) // it wll skip first 3 postion and return all string
+ * echo substr($email, 0, 5) // it wll return only 0 to 5 string
+ 
+ * echo sub_suffle($email, 0, 5) // get random
+ * echo strip_tags($var) // remove html tgas from $var
+ * echo trim($var) // remove html tgas from $var
+ *
+ * password_hash($pass, PASSWORD_DEFAULT); // hash the pass
+ *
+ * password_verify($pass, $hash_pass); // return true if match
+ *
+*/
 ?>
