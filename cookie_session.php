7a3d90bf3d1a9c1d19e214d20621b09229ef453f
usernamedinesh <borod9200@gmail.com>
2025-08-02 17:15:27 +0530

    session done

--- cookie_session.php
+++ cookie_session.php
@@ -11,9 +11,27 @@ echo "cookie & session super global variable\n";
   
 */ 
 
-/*
+/*  ===========COOKIE=============
  * cookie : is a small amount of data that we can store in brower(destoyed when close browser)(not sucure)
   set cookie: setcookie("name" "value"); 
   get cookie: $_COOKIE["name"]; 
+
+/*  ===========SESSION=============
+ *  session: is a huge amount of data store in a server (file)
+ 
+ * create an session*
+   session_start();
+   $_SESSION["nam"] = "value";
+
+ * accessing an session*
+   session_start();
+   echo $_SESSION["nam"];
+
+ * deleting an session*
+   session_start();
+   session_unset();   // remove session data from session file
+   session_destroy(); // remove the session file permanently
+// after session is created the server will send the session_id to browser  and the browser save it as a cookie 
+  
 */ 
 ?>
